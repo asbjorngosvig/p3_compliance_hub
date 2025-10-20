6c77c60db13870ec3fee98b1767ed14eb2bff647
Marcus Linde <181730171+marcuslinde@users.noreply.github.com>
2025-10-20 22:25:35 +0200

    UserService
    - created file
    - initialised the first function. proper working, but lacks DTO, which needs to be setup first. hence commenting
    - imported spring security to pom, to be used in service

--- backend/src/main/java/com/compliancehub/service/UserService.java
+++ backend/src/main/java/com/compliancehub/service/UserService.java
@@ -1,5 +1,41 @@
 package com.compliancehub.service;
 
+import com.compliancehub.model.Admin;
+import com.compliancehub.model.Employee;
+import com.compliancehub.model.User;
+import com.compliancehub.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import org.springframework.security.access.AccessDeniedException;
+
+@RequiredArgsConstructor // Lombok laver en constructor med final felter (altså userRepository) automatisk
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    /*
+    public User createUser(User caller, CreateUserDto dto) {
+        if (!caller.isAdmin()) {
+            throw new AccessDeniedException("Only admins can create users");
+        }
+
+        User newUser = "ADMIN".equalsIgnoreCase(dto.role())
+                ? new Admin(dto.name(), dto.email(), dto.password())
+                : new Employee(dto.name(), dto.email(), dto.password());
+
+        return userRepository.save(newUser);
+    }*/
+
 
 }
+
+
+
+/*
+Opret bruger
+
+Tjek rolle
+
+Hash password
+
+Valider input*/
