ca657700a2fbd703d4a228c99176006a0a496132
asbjorngosvig <asbjorn.gosvig@gmail.com>
2025-10-22 11:02:56 +0200

    added tailwindcss

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../shared/styles/index.css'
 import App from './App.tsx'
+import "../shared/styles/App.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
