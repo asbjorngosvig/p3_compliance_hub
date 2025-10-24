c40d1df7e4d5d8ae80f1a01adcfdeddcf0b485a0
pandajakob <jakobm.dev@gmail.com>
2025-10-24 10:41:14 +0200

    Added get user by id functionality

--- backend/src/main/java/com/compliancehub/controller/UserController.java
+++ backend/src/main/java/com/compliancehub/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.compliancehub.controller;
 
+import com.compliancehub.dto.user.UserGetResponse;
+import com.compliancehub.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    UserService service;
+    @GetMapping("/{id}")
+    private ResponseEntity<UserGetResponse> getUserById(@PathVariable long id) {
+        UserGetResponse user = service.getById(id);
+
+        URI location = URI.create("/users/" + user.id());
+
+        // 3. Return the response using the res-DTO
+        return ResponseEntity.created(location).body(user);
+    }
+
 }
--- backend/src/main/java/com/compliancehub/service/UserService.java
+++ backend/src/main/java/com/compliancehub/service/UserService.java
@@ -1,13 +1,31 @@
 package com.compliancehub.service;
 
+import com.compliancehub.dto.user.UserGetResponse;
+import com.compliancehub.model.User;
 import com.compliancehub.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.InputMismatchException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-    // Her kan du tilføje dine metoder senere
+    public UserGetResponse getById(long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        // make sure that user exists before returning
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            return new UserGetResponse(user.getId(), user.getEmail(), user.getName(), user.getRole());
+        } else {
+            throw new InputMismatchException("Could not find user with id: " +  id);
+        }
+    }
+
+
+
 }
