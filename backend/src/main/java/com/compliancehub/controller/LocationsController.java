c20275f79e5e5358112d905071993f94f75a32a6
pandajakob <jakobm.dev@gmail.com>
2025-10-25 10:47:53 +0200

    Tilføjet en location controller

---
+++ backend/src/main/java/com/compliancehub/controller/LocationsController.java
@@ -0,0 +1,38 @@
+package com.compliancehub.controller;
+
+import com.compliancehub.utils.Locations;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/locations")
+
+public class LocationsController {
+
+    @GetMapping
+    Set<String> getAllLocations() {
+        return Locations.locations;
+    }
+
+    // Returnere alle lande som indeholder den streng der kommer ind. gør det muligt at søge.
+    @GetMapping("/{name}")
+    List<String> getLocationsWhereNameIs(@PathVariable String name) {
+        List<String> newArr = new ArrayList<>();
+        Locations.locations.forEach((loc)->{
+            if (loc.contains(name.toUpperCase())) {
+                newArr.add(loc);
+            }
+        });
+        return newArr;
+    }
+
+}
+
