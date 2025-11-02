658cdf6da0022765b943cde44709ce33319f4916
nichzar <nichzar19@gmail.com>
2025-11-02 15:49:16 +0100

    added testing of DataProcessorControllerTest getById

--- backend/src/test/java/com/compliancehub/controller/DataProcessorControllerTest.java
+++ backend/src/test/java/com/compliancehub/controller/DataProcessorControllerTest.java
@@ -1,6 +1,8 @@
 package com.compliancehub.controller;
 
+import com.compliancehub.model.DataProcessor;
 import com.compliancehub.repository.DataProcessorRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,35 +22,24 @@ class DataProcessorControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    /*
     @Autowired
     private DataProcessorRepository dataProcessorRepository;
 
-    @BeforeEach
-    void setUp() {
-        DataProcessor dataProcessor = new DataProcessor();
-        dataProcessor.setName("testProcessor");
-        dataProcessor.setHosting_location("USA");
-        dataProcessor.setService("cloudStorage");
-        dataProcessor.setPurpose("dataBackup");
-        dataProcessor.setNote("testNote");
-        dataProcessor.setWebsite("https://example.com");
-
-        dataProcessorRepository.deleteAll();
-        dataProcessorRepository.save(dataProcessor);
-    }
-    */
 
     @Test
-    void testCreateDataProcessor() throws Exception {
-    mockMvc.perform(post("/dataprocessors/")
-    .contentType(MediaType.APPLICATION_JSON)
-    .content("{\"name\":\"testName\","+
-        "\"hosting_location\":\"testLocation\","+
-        "\"service\":\"testService\","+
-        "\"purpose\":\"testPurpose\","+
-        "\"note\":\"testNote\","+
-        "\"website\":\"testWebsite\"}"))
+    void testGetDataProcessorByid() throws Exception {
+        dataProcessorRepository.deleteAll();
+        DataProcessor dataProcessor = new DataProcessor();
+        dataProcessor.setName("testProcessor");
+        dataProcessor.setHosting_location("testLocation");
+        dataProcessor.setService("testService");
+        dataProcessor.setPurpose("testPurpose");
+        dataProcessor.setNote("testNote");
+        dataProcessor.setWebsite("testWebsite");
+
+        dataProcessorRepository.save(dataProcessor);
+
+        mockMvc.perform(get("/dataprocessors/1"))
 
         .andExpect(status().isCreated())
         .andExpect(jsonPath("$.id").value(1L))
@@ -58,4 +49,26 @@ class DataProcessorControllerTest {
         .andExpect(jsonPath("$.note").value("testNote"))
         .andExpect(jsonPath("$.website").value("testWebsite"));
     }
+
+    @Test
+    void testCreateDataProcessor() throws Exception {
+        mockMvc.perform(post("/dataprocessors/")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"name\":\"testName\","+
+            "\"hosting_location\":\"testLocation\","+
+            "\"service\":\"testService\","+
+            "\"purpose\":\"testPurpose\","+
+            "\"note\":\"testNote\","+
+            "\"website\":\"testWebsite\"}"))
+
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id").value(2L)) //Running 2l instead of 1 due to it already being created earlier in test of getById test
+        .andExpect(jsonPath("$.hosting_location").value("testLocation"))
+        .andExpect(jsonPath("$.service").value("testService"))
+        .andExpect(jsonPath("$.purpose").value("testPurpose"))
+        .andExpect(jsonPath("$.note").value("testNote"))
+        .andExpect(jsonPath("$.website").value("testWebsite"));
+    }
+
+
 }
