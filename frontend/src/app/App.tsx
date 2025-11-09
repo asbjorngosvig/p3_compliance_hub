eeda11ad7789abbf4045c417deb49f2faa6f8ea3
RyanZachariasen <143599102+RyanZachariasen@users.noreply.github.com>
2025-11-09 17:30:53 +0100

    -fixed app -fixed cards text alignment
    Issues : tailwind import?

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,7 +1,6 @@
 import '../shared/styles/App.css'
 import {OverviewHeaderComponent} from '../shared/components/OverviewHeaderComponent.tsx';
 import '../shared/styles/App.css'
-import {Button} from '../shared/components/Buttons';
 import Sidebar from '../shared/components/Sidebar';
 import {useState} from 'react'
 
