17881ee3a9a5ec5ddae6bd07161d952b5cdadd99
RohanAtik <hanhan@live.dk>
2025-11-12 14:17:45 +0100

    Tilføjet Dpaform og opdateret App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,34 +1,26 @@
-import '../shared/styles/App.css'
-import {OverviewHeader} from '../shared/components/OverviewHeader.tsx';
-import '../shared/styles/App.css'
-import Sidebar from '../shared/components/Sidebar';
-import {useState} from 'react'
+import { useState } from "react";
+import '../shared/styles/App.css';
+import OverviewHeaderComponent from '../shared/components/OverviewHeader'; // default
+import Sidebar from '../shared/components/Sidebar';                               // default
+import Dpaform from '../features/add dpa/Dpaform'; // eller '../features/add-dpa/Dpaform'
 
 function App() {
-    // const [count, setCount] = useState(0);
-    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false)
+    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
     return (
         <>
+            <Sidebar
+                isCollapsed={isSidebarCollapsed}
+                toggle={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
+            />
 
-            <div className="flex h-screen">
-                <Sidebar
-                    isCollapsed={isSidebarCollapsed}
-                    toggle={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
-                />
+            <OverviewHeaderComponent />
 
-                <div
-                    className={`flex-1 transition-all duration-300 ${
-                        isSidebarCollapsed ? 'pl-16' : 'pl-64'} xl:-mx-20 md:-mx-0`}
-                >
-
-                    <OverviewHeader />
-
-
-                </div>
+            <div className="flex justify-center items-start mt-10">
+                <Dpaform />
             </div>
+        </>
+    );
+}
 
-            </>
-            )
-            }
-
-            export default App;
+export default App;
