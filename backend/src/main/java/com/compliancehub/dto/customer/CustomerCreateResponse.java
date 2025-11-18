4fea7529158943a2c448e5fee11040d275ea5bbc
pandajakob <jakobm.dev@gmail.com>
2025-11-18 11:46:50 +0100

    remove old classes

--- backend/src/main/java/com/compliancehub/dto/customer/CustomerCreateResponse.java
+++
@@ -1,6 +0,0 @@
-package com.compliancehub.dto.customer;
-
-// Bruges til at returnere det nye ID efter oprettelse
-public record CustomerCreateResponse(
-        Long id
-) {}
--- backend/src/test/java/com/compliancehub/controller/CustomerControllerTest.java
+++ backend/src/test/java/com/compliancehub/controller/CustomerControllerTest.java
@@ -1,6 +1,5 @@
 package com.compliancehub.controller;
 
-import com.compliancehub.service.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
