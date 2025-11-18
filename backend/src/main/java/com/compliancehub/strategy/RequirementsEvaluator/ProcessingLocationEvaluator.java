b2a2947350b13d13995cee3e1687cd199ff73aa2
pandajakob <jakobm.dev@gmail.com>
2025-11-18 13:28:53 +0100

    add parsing for attributes

--- backend/src/main/java/com/compliancehub/strategy/RequirementsEvaluator/ProcessingLocationEvaluator.java
+++ backend/src/main/java/com/compliancehub/strategy/RequirementsEvaluator/ProcessingLocationEvaluator.java
@@ -2,11 +2,27 @@ package com.compliancehub.strategy.RequirementsEvaluator;
 
 import com.compliancehub.model.DPA;
 import com.compliancehub.model.DataProcessor;
+import com.compliancehub.model.Requirement;
 import com.compliancehub.model.Violation;
+
+import java.util.List;
+import java.util.Map;
 
 public class ProcessingLocationEvaluator implements RequirementsEvaluator {
     @Override
     public Violation evaluate(DPA dpa, DataProcessor dataProcessor) {
         return null;
     }
+
+    @Override
+    public Map<String, Object> createAttributesMap() {
+
+    }
+
+    @Override
+    public void parseAttributes(Map<String, Object> attributes) {
+
+    }
+
+
 }
--- backend/src/main/java/com/compliancehub/strategy/RequirementsEvaluator/RequirementsEvaluator.java
+++ backend/src/main/java/com/compliancehub/strategy/RequirementsEvaluator/RequirementsEvaluator.java
@@ -5,6 +5,14 @@ import com.compliancehub.model.DPA;
 import com.compliancehub.model.DataProcessor;
 import com.compliancehub.model.Violation;
 
+import java.util.Map;
+
 public interface RequirementsEvaluator {
     Violation evaluate(DPA dpa, DataProcessor dataProcessor);
+
+
+    Map<String, Object> createAttributesMap();
+
+    void parseAttributes( Map<String, Object> attributes);
+
 }
