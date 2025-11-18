cd8d3dbcf99ed7c1fc71cee6afb621ec5a052ad1
pandajakob <jakobm.dev@gmail.com>
2025-11-18 13:17:58 +0100

    Communication Strategy + test files

---
+++ backend/src/main/java/com/compliancehub/strategy/CommunicationStrategy/NeedWrittenAprooval.java
@@ -0,0 +1,33 @@
+package com.compliancehub.strategy.CommunicationStrategy;
+
+import com.compliancehub.model.Action;
+import com.compliancehub.model.DPA;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NeedWrittenAprooval implements CommuncationStrategy{
+    public String email;
+
+    @Override
+    public Action createAction(DPA dpa) {
+        Action newAction = new  Action();
+        newAction.setDescription("You need written approval from " + dpa.getCustomerName() + " before changing data processor");
+        return newAction;
+    }
+
+    @Override
+    public Map<String, Object> createAttributeMap() {
+        Map<String, Object> newMap = new HashMap<>();
+
+        // put all attributes here
+        newMap.put("email", email);
+
+        return newMap;
+    }
+
+    @Override
+    public void parseAttributes(Map<String, Object> attributes) {
+        this.email = (String) attributes.get("email");
+    }
+}
---
+++ backend/src/test/java/com/compliancehub/Strategy/CommunicationStrategy/NeedWrittenAproovalTest.java
@@ -0,0 +1,7 @@
+package com.compliancehub.Strategy.CommunicationStrategy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NeedWrittenAproovalTest {
+
+}
