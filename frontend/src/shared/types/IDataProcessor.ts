aaa7355855a523150d12d43f5ee0035a710a3af3
RyanZachariasen <143599102+RyanZachariasen@users.noreply.github.com>
2025-11-24 16:06:22 +0100

    -Dataprocessors    - Service and Types

---
+++ frontend/src/shared/types/IDataProcessor.ts
@@ -0,0 +1,11 @@
+export interface IDataProcessor {
+    id?: string;
+    name: string;
+    processingLocation: string[];
+    service: string;
+    purpose: string;
+    note: string;
+    website: string;
+}
+
+
