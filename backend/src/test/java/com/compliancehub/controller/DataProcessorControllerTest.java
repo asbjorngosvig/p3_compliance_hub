40addff55798e490a63d63bbe9bc0dc5b165e039
nichzar <nichzar19@gmail.com>
2025-11-24 11:01:20 +0100

    Fixed testing with security

--- backend/src/test/java/com/compliancehub/controller/DataProcessorControllerTest.java
+++ backend/src/test/java/com/compliancehub/controller/DataProcessorControllerTest.java
@@ -4,6 +4,8 @@ import com.compliancehub.model.DataProcessor;
 import com.compliancehub.repository.DataProcessorRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -15,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 class DataProcessorControllerTest {
 
     @Autowired
