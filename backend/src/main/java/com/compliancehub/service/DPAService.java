7bbc5cd554231abf091b37decabed5b474de8c52
pandajakob <jakobm.dev@gmail.com>
2025-11-26 12:38:54 +0100

    Parser for requirement evaluator

--- backend/src/main/java/com/compliancehub/service/DPAService.java
+++ backend/src/main/java/com/compliancehub/service/DPAService.java
@@ -1,10 +1,25 @@
 package com.compliancehub.service;
 
 
+import com.compliancehub.model.DPA;
+import com.compliancehub.model.Requirement;
+import com.compliancehub.strategy.RequirementsEvaluator.ProcessingLocationEvaluator;
+import com.compliancehub.strategy.RequirementsEvaluator.RequirementsEvaluator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class DPAService {
+
+
+    private RequirementsEvaluator getReqEvalutorFromString(Requirement requirement) {
+
+        switch (requirement.getReqEvaluator()) {
+            case "ProcessingLocationsEvaluator": return new ProcessingLocationEvaluator(requirement.getAttributes());
+            // todo: Add more Evaluators
+            default: throw new RuntimeException("Error getting evaluator "+requirement.getReqEvaluator());
+        }
+    }
+
 }
