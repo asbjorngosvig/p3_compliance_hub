0ce300bde60034a1eac88c9a772f01949d0004cd
pandajakob <jakobm.dev@gmail.com>
2025-12-01 11:01:28 +0100

    change cors setting

--- backend/src/main/java/com/compliancehub/Security/JwtFilter.java
+++ backend/src/main/java/com/compliancehub/Security/JwtFilter.java
@@ -31,12 +31,11 @@ public class JwtFilter extends OncePerRequestFilter {
 
         String path = request.getRequestURI();
 
-        if (path.equals("/api/users/login") || path.equals("/api/users/register")) {
+        if (path.equals("/api/users/login") || path.equals("/api/users/register") || request.getMethod().equalsIgnoreCase("OPTIONS") ) {
             filterChain.doFilter(request, response);
             return;
         }
-
-
+        
         String token = request.getHeader("Cookie").substring(4);
         String userEmail;
 
