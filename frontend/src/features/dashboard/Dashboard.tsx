8ac0c4173d42a5484da05b5a1b75b6d93db080bd
asbjorngosvig <asbjorn.gosvig@gmail.com>
2025-12-08 15:30:38 +0100

    overviewheader real data

--- frontend/src/features/dashboard/Dashboard.tsx
+++ frontend/src/features/dashboard/Dashboard.tsx
@@ -1,10 +1,8 @@
-import { OverviewHeader } from "../../shared/components/OverviewHeader.tsx";
 import DpaOverview from "../dpas/DpaOverview";
 
 export function Dashboard() {
     return (
         <div className="p-6 space-y-6">
-            <OverviewHeader />
             <DpaOverview />
         </div>
     );
