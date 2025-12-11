666d6e3abd1e684fcfcdf5d81bf6887816767529
pandajakob <jakobm.dev@gmail.com>
2025-12-11 14:04:34 +0100

    check if logged in

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,13 +1,29 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routers";
 import {ConfirmProvider} from "../shared/components/ConfirmDialog.tsx";
+import { useState } from "react";
+import Loader from "../shared/components/Loader.tsx";
 
 export default function App() {
 
+  const [isLoading,setIsLoading] = useState(false);
+  async function login(){
+      try {
+        
+          await fetch("http://localhost:8080/api/users")
+      } catch {
+
+      }
+  }
+  if (isLoading) {
+    return Loader;
+  }
   return(
+    <div onChange={login}>
       <ConfirmProvider>
       <RouterProvider router={router} />
       </ConfirmProvider>
+  </div>
   );
 }
 
