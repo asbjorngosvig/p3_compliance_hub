52d2ff4fafcab60ab614227b8016e2d1ca0ecf8a
Marcus Linde <181730171+marcuslinde@users.noreply.github.com>
2025-12-15 14:29:59 +0100

    created build()
    - created build() method to finish builder pattern off. finish that

--- backend/src/main/java/com/compliancehub/service/builder/DPABuilder.java
+++ backend/src/main/java/com/compliancehub/service/builder/DPABuilder.java
@@ -9,17 +9,6 @@ import java.util.List;
 import java.util.Map;
 
 public class DPABuilder {
-    /*
-    1) set customer name
-    2) set product name
-    3) set file URL
-    4) set comm strats (kun nogen gange. også alle typer både måden at få info på og hvornår man skal ha den)
-    5) set requirements
-    6) return dpa
-
-    herefter fikser service eller observer at lave violations
-     */
-
     private DPA dpa;
 
     public DPABuilder (String customerName, String productName, String url){
@@ -67,5 +56,9 @@ public class DPABuilder {
         this.dpa.addRequirement(r);
 
         return this;
+    }
+
+    public DPA build(){
+        return this.dpa;
     }
 }
